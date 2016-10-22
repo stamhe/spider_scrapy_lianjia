280e884d70509608c36ae8677fd9e1644bfa1313
Stam He <stamhe@163.com>
2016-10-22 14:47:42 +0800

    2016-10-22

--- spider_scrapy_lianjia/spiders/lianjiaspider.py
+++ spider_scrapy_lianjia/spiders/lianjiaspider.py
@@ -23,8 +23,62 @@ class lianjia_spider(CrawlSpider):
 
     # 爬取的url列表，爬虫从这里开始抓取数据，所以，第一次下载的数据将会从这些urls开始，其他子url将会从这些起始url中继承性生成
     start_urls = [
+        # 新里维多利亚 1611043078432
         'http://cd.lianjia.com/ershoufang/pg1c1611043078432/',
         'http://cd.lianjia.com/ershoufang/pg2c1611043078432/',
+        # 英郡一期 3011055403435
+        'http://cd.lianjia.com/ershoufang/pg1c3011055403435/',
+        'http://cd.lianjia.com/ershoufang/pg2c3011055403435/',
+        # 英郡三期 3011055400648
+        'http://cd.lianjia.com/ershoufang/c3011055400648/',
+        # 英郡二期
+        'http://cd.lianjia.com/ershoufang/c3011055399653/',
+        # 华润二十四城一期
+        'http://cd.lianjia.com/ershoufang/pg1c3011054427433/',
+        'http://cd.lianjia.com/ershoufang/pg2c3011054427433/',
+        'http://cd.lianjia.com/ershoufang/pg3c3011054427433/',
+        # 华润二十四城二期
+        'http://cd.lianjia.com/ershoufang/pg1c3011055110938/',
+        'http://cd.lianjia.com/ershoufang/pg2c3011055110938/',
+        'http://cd.lianjia.com/ershoufang/pg3c3011055110938/',
+        'http://cd.lianjia.com/ershoufang/pg4c3011055110938/',
+        # 华润二十四城三期
+        'http://cd.lianjia.com/ershoufang/pg1c3011055107784/',
+        'http://cd.lianjia.com/ershoufang/pg2c3011055107784/',
+        # 桐梓林壹号
+        'http://cd.lianjia.com/ershoufang/c1611051173541/',
+        # 中德英伦联邦A区
+        'http://cd.lianjia.com/ershoufang/pg1c1611059031041/',
+        'http://cd.lianjia.com/ershoufang/pg2c1611059031041/',
+
+        # 中德英伦联邦B区
+        'http://cd.lianjia.com/ershoufang/c1611061209751/',
+        # 仁和春天国际花园
+        'http://cd.lianjia.com/ershoufang/c1611047388205',
+        # 时代晶科名苑
+        'http://cd.lianjia.com/ershoufang/c1611043113553/',
+        # 中海龙湾半岛
+        'http://cd.lianjia.com/ershoufang/pg1c1611041535394/',
+        'http://cd.lianjia.com/ershoufang/pg2c1611041535394/',
+        'http://cd.lianjia.com/ershoufang/pg3c1611041535394/',
+        'http://cd.lianjia.com/ershoufang/pg4c1611041535394/',
+        # 中铁西子香荷
+        'http://cd.lianjia.com/ershoufang/c1611041555455/',
+        # 朗基龙堂
+        'http://cd.lianjia.com/ershoufang/pg1c1611041619788/',
+        'http://cd.lianjia.com/ershoufang/pg2c1611041619788/',
+        # 南城都汇汇翠园一期
+        'http://cd.lianjia.com/ershoufang/pg1c1611047102608/',
+        'http://cd.lianjia.com/ershoufang/pg2c1611047102608/',
+        # 南城都汇汇雅园二期
+        'http://cd.lianjia.com/ershoufang/pg1c1611043169737/',
+        'http://cd.lianjia.com/ershoufang/pg2c1611043169737/',
+        # 南城都汇汇馨园三期
+        'http://cd.lianjia.com/ershoufang/c1611047199429/',
+        # 南城都汇汇尚园四期
+        'http://cd.lianjia.com/ershoufang/c1611047199430/',
+        # 中海兰庭
+        'http://cd.lianjia.com/ershoufang/c1611047737374/',
     ]
 
     # 解析的方法，调用的时候传入从每一个url传回的response对象作为唯一参数，负责解析并获取抓取的数据(解析为item)，跟踪更多的url
