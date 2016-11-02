25db9acfc37dfaf99c5fc4a321d927496788cb38
root <root@10-9-91-150.(none)>
2016-11-02 23:42:12 +0800

    fix bug

--- spider_run.sh
+++ spider_run.sh
@@ -2,7 +2,7 @@
 
 workdir=/root/hequan/spider_scrapy_lianjia/spider_scrapy_lianjia
 cd ${workdir}
-/usr/bin/python /usr/local/bin/scrapy crawl lianjia_house_spider
-/usr/bin/python /usr/local/bin/scrapy crawl chenshi_count_spider
+/usr/bin/python /usr/local/python/bin/scrapy crawl lianjia_house_spider
+/usr/bin/python /usr/local/python/bin/scrapy crawl chenshi_count_spider
 
 
