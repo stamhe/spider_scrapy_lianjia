18a193371366f7d378f6669f4d01003eb22c0487
hequan-uat <hequan@rrr.me>
2017-08-09 14:30:36 +0800

    renew

--- spider_scrapy_lianjia/settings.py
+++ spider_scrapy_lianjia/settings.py
@@ -82,7 +82,7 @@ COOKIES_ENABLED = False
 
 # 是否可以重试
 RETRY_ENABLED = True
-RETRY_TIMES = 5
+RETRY_TIMES = 10
 RETRY_HTTP_CODES = [407,500,502,503,504, 403]
 
 # Enable or disable downloader middlewares
